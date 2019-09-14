7e8e69fc9f599f64026408295b8199cc0419ee0b
Ross Mckay <ross.mckay1975@gmail.com>
2019-09-14 16:12:58 +0100

    newfolder

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>The World of Ghibli</h1>
+    <div class="main-container">
+      <films-list :films="films"></films-list>
+      <film-detail :film="selectedFilm"></film-detail>
+    </div>
+  </div>
+</template>
+
+<script>
+import { eventBus } from './main.js'
+import FilmsList from './components/FilmsList.vue';
+import FilmDetail from './components/FilmDetail.vue'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      films: [],
+      selectedFilm: null
+      }
+    },
+    components: {
+      "films-list": FilmsList,
+      "film-detail": FilmDetail
+    },
+  mounted(){
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(res => res.json())
+    .then(films => this.films = films)
+    
+      eventBus.$on('film-selected', (film) => {
+        this.selectedFilm = film
+      })
+    }
+  }
+
+</script>
+
+<style>
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/FilmDetail.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+  <div v-if="film" id="filmDetail">
+    <div id="detailWrapper">
+    <h2>{{ film.title }}</h2>
+    <div id="flexWrapper">
+    </div>
+      <div id="left">
+        <p><span>Film Description</span>: {{ film.description }} </p>
+        <p><span>Must Watch</span>: </p>
+
+      </div>
+      <div id="right">
+        <button type="button" v-on:click="makeFavourite">Make Favourite</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'film-detail',
+  props: ['film'],
+  methods: {
+    makeFavourite: function(){
+      this.film.favourite = true
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+  ul {
+    list-style-type: none;
+    }
+
+  .button {
+    background-color: #4CAF50;
+  }
+
+
+
+</style>
